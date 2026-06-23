9561efe8c957332f09d636068043c98c02178481
alok yadav <alok842959@gmail.com>
2026-06-23 17:20:12 +0530

    Added all experiments and lab exam files successfully

---
+++ Experiments/Exp5/EXp5/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function App(){
+ const[score,setscore]=usestate(0);
+ return(
+  <div>
+    <h1>welcome to the game zone</h1>
+    <h2>current count:{0}</h2>
+    <button onclick = {   }
+  </div>
+ )
+}
